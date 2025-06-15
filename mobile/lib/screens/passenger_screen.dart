69ed92ed72050177da5a62f8efc8980a5c771d57
TunahanDso <89835977+TunahanDso@users.noreply.github.com>
2025-06-16 02:16:02 +0300

    Add PassengerScreen widget and test

---
+++ mobile/lib/screens/passenger_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PassengerScreen extends StatelessWidget {
+  const PassengerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Passenger Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {},
+          child: const Text('Request Ride'),
+        ),
+      ),
+    );
+  }
+}
